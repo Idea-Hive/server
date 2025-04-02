7b56e6114d96b40339b200b1c7da3f92c798a469
indoor98 <98157935+indoor98@users.noreply.github.com>
2025-04-02 20:43:03 +0900

    제목(subject): - 필요 라이브러리 추가 (build.gradle) - Spring Security Config 초안 - Member Entity 초안
    본문(body):
    - 생략
    
    푸터(footer):
    - 생략

--- src/main/java/Idea/Idea_Hive/member/entity/Member.java
+++ src/main/java/Idea/Idea_Hive/member/entity/Member.java
@@ -1,4 +1,52 @@
 package Idea.Idea_Hive.member.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+/**
+ * Member Entity
+ * 데이터 삭제 시 컬럼 값으로만 처리, 실제 데이터는 DB에 남아있도록 하였음.
+ * 컨벤션 : Entity 생성자는 Builder 패턴 사용으로 고정하면 좋을 것 같습니다.
+ */
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE member SET is_deleted = true WHERE id = ?") // Soft Delete
+@SQLRestriction("is_deleted = false")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdDate;
+
+    /* todo: 소셜 로그인 관련 필드 추가 */
+    /* todo: 회원 정보 변경 시 수정 시간 필드 필요 시 추가 */
+
+    private Boolean isDeleted;
+
+
+    @Builder
+    public Member(final String name, final String email, final String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdDate = LocalDateTime.now();
+        this.isDeleted = false;
+    }
 }
