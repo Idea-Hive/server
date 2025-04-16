404ffca6f8ebb7018a3384a31bdfa178f2760961
soyeun <thduswkd96@gmail.com>
2025-04-16 17:44:45 +0900

    chore: entity creation modified

---
+++ src/main/java/Idea/Idea_Hive/task/entity/ProjectTask.java
@@ -0,0 +1,17 @@
+package Idea.Idea_Hive.task.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "project_task")
+public class ProjectTask {
+
+    @EmbeddedId
+    private ProjectTaskId id;
+}
