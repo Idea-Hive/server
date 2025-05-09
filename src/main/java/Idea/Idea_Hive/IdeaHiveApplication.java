34ef33834e8532c74522897f7a343d0807f62396
soyeun <thduswkd96@gmail.com>
2025-05-09 22:15:56 +0900

    chore: .env setting

--- src/main/java/Idea/Idea_Hive/IdeaHiveApplication.java
+++ src/main/java/Idea/Idea_Hive/IdeaHiveApplication.java
@@ -1,9 +1,12 @@
 package Idea.Idea_Hive;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:.env")
 public class IdeaHiveApplication {
 
 	public static void main(String[] args) {
