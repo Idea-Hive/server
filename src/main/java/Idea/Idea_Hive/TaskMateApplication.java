e17f223fb00b8131e1cd118a721cb313449c183f
indoor98 <98157935+indoor98@users.noreply.github.com>
2025-05-10 11:30:01 +0900

    PostgreSQL docker-compose.yml 파일 추가

---
+++ src/main/java/Idea/Idea_Hive/TaskMateApplication.java
@@ -0,0 +1,16 @@
+package Idea.Idea_Hive;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+//@PropertySource(value = "file:.env")
+public class IdeaHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaHiveApplication.class, args);
+	}
+
+}
