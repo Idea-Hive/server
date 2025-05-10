21ad4a1d5f87eca18705c7f8d55197dc160e36d1
indoor98 <98157935+indoor98@users.noreply.github.com>
2025-05-10 11:35:24 +0900

    postgresql docker-compose.yml 작성

--- src/main/java/Idea/Idea_Hive/TaskMateApplication.java
+++ src/main/java/Idea/Idea_Hive/TaskMateApplication.java
@@ -1,16 +1,14 @@
 package Idea.Idea_Hive;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 //@PropertySource(value = "file:.env")
-public class IdeaHiveApplication {
+public class TaskMateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdeaHiveApplication.class, args);
+		SpringApplication.run(TaskMateApplication.class, args);
 	}
 
 }
--- src/test/java/Idea/Idea_Hive/TaskMateApplicationTests.java
+++ src/test/java/Idea/Idea_Hive/TaskMateApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IdeaHiveApplicationTests {
+class TaskMateApplicationTests {
 
 	@Test
 	void contextLoads() {
