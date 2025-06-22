210b1584272541d6a4148bc1d2325eab0a566c10
soyeun <thduswkd96@gmail.com>
2025-06-22 18:51:39 +0900

    chore: notifications reponse 내용 변경 및 pagination

--- src/main/java/Idea/Idea_Hive/notification/controller/NotificationController.java
+++ src/main/java/Idea/Idea_Hive/notification/controller/NotificationController.java
@@ -1,15 +1,18 @@
 package Idea.Idea_Hive.notification.controller;
 
+import Idea.Idea_Hive.notification.dto.NotificationDto;
 import Idea.Idea_Hive.notification.entity.Notification;
 import Idea.Idea_Hive.notification.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -20,8 +23,11 @@ public class NotificationController {
 
     @Operation(summary = "알림 목록 조회")
     @GetMapping
-    public ResponseEntity<List<Notification>> getNotifications(@RequestParam(required = true) Long userId) {
-        return ResponseEntity.ok(notificationService.getNotifications(userId));
+    public ResponseEntity<List<NotificationDto>> getNotifications(@RequestParam(required = true) Long userId,
+                                                                  @RequestParam(defaultValue = "1") int page,
+                                                                  @RequestParam(defaultValue = "3") int size)
+    {
+        return ResponseEntity.ok(notificationService.getNotifications(userId, page, size));
     }
 
     @Operation(summary = "읽지 않은 알림 개수 조회")
