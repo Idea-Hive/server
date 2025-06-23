aa1499643abff5efba0245f9467aa4d065468d1b
soyeun <thduswkd96@gmail.com>
2025-06-23 18:09:29 +0900

    chore: s3 connection

--- src/main/java/Idea/Idea_Hive/file/service/FileStorageService.java
+++ src/main/java/Idea/Idea_Hive/file/service/FileStorageService.java
@@ -1,8 +1,10 @@
 package Idea.Idea_Hive.file.service;
 
 import Idea.Idea_Hive.task.dto.response.TaskResponse;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.multipart.MultipartFile;
 
+@Profile("local")
 public interface FileStorageService {
 
     TaskResponse storeFile(MultipartFile file, Long taskId); // 저장 후 파일 접근 경로 또는 키 반환
